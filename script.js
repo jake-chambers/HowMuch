08ff9cdcb0d6c7f7e9f56f7e2e4604ef7ccab055
Jacob <jake_chambers12@hotmail.com>
2019-07-15 21:00:44 -0400

    datepicker done

--- script.js
+++ script.js
@@ -7,48 +7,55 @@ today = yyyy + '-' + mm + '-' + dd;
 console.log(today);
 
 
-function getInfo(){
+function getInfo() {
     ticker = document.getElementById("tickerInput").value;
-    date = document.getElementById("dateInput").value;
+    date = document.getElementById("datepicker").value;
+    numShares = document.getElementById("numberOfShares").value
+
+
+    if (!ticker || !date || !numShares){
+        alert('Fill in all the fields.')
+    }
 
     body = {
         ticker: ticker
     }
 
-    axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&apikey=MT81UG9QNO8YFAJN&outputsize=full', body)
+    axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker + '&apikey=MT81UG9QNO8YFAJN&outputsize=full', body)
         .then(data => {
             let tickerThen = data.data['Time Series (Daily)'][date];
-            console.log(tickerThen)
             let tickerNow = data.data['Time Series (Daily)'][today];
             let priceThen = tickerThen['4. close'];
             let priceNow = tickerNow['4. close']
-            numShares = document.getElementById("numberOfShares").value
-            document.getElementById('result').innerHTML = '<p id = "test">If you bought ' + numShares + ' shares of ' + ticker + ' on ' + date + ', it is now worth <i>approximately</i> : <br><br><span id = "number">$'+calculateEarnings(priceThen, priceNow, numShares)+'</span></p>';
+
+            document.getElementById('result').innerHTML = '<p>If you bought ' + numShares + ' shares of ' + ticker + ' on ' + date + ', it is now worth <i>approximately</i> : <br><br><span id = "number">$' + calculateEarnings(priceThen, priceNow, numShares) + '</span></p>';
+
         })
         .catch(err => {
             console.log(err);
+            document.getElementById('result').innerHTML = '<p>Sorry! The stock information for that date cannot be retrieved :(. Please double check when the company IPO\'d or pick a different date.</p>';
         });
 }
 
-function calculateEarnings(priceThen, priceNow,numShares){
-    let delta = parseInt(priceNow)/parseInt(priceThen);
+function calculateEarnings(priceThen, priceNow, numShares) {
+    let delta = parseInt(priceNow) / parseInt(priceThen);
     let result;
 
-    if (delta < 1){
-        console.log('Stock has decreased by %' + delta*100);
-        result = priceThen - (priceThen*delta);
+    if (delta < 1) {
+        console.log('Stock has decreased by %' + delta * 100);
+        result = priceThen - (priceThen * delta);
     }
 
-    else if (delta > 1){
-        console.log('Stock has increased by %' + delta*100)
-        result =  (priceThen * delta);
+    else if (delta > 1) {
+        console.log('Stock has increased by %' + delta * 100)
+        result = (priceThen * delta);
     }
 
-    else if (delta == 1){
+    else if (delta == 1) {
         console.log('Stock price has not changed.')
-    }  
+    }
 
-    result = result*parseInt(numShares);
+    result = result * parseInt(numShares);
     console.log(result);
     result = Number(Math.round(result + 'e2') + 'e-2');
     return result;
